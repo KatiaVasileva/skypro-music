afe6620ac5482a7f8bf001cec826d0b967d3e3c4
KatiaVasileva <eka.vasileva@gmail.com>
2024-08-15 21:01:05 +0500

    side menu is opened/closed by clicking the burger

--- skypro-music/src/components/Nav/Nav.tsx
+++ skypro-music/src/components/Nav/Nav.tsx
@@ -1,7 +1,16 @@
+"use client";
+
 import Image from "next/image";
-import styles from "./Nav.module.css"
+import styles from "./Nav.module.css";
+import React, { useState } from "react";
 
 function Nav() {
+  const [isBurgerClicked, setIsBurgerClicked] = useState<boolean>(false);
+
+  const handleBurgerClick: React.MouseEventHandler<HTMLDivElement> = () => {
+    setIsBurgerClicked((prevState) => !prevState);
+  };
+
   return (
     <nav className={styles.main}>
       <div className={styles.logo}>
@@ -13,30 +22,32 @@ function Nav() {
           height={17}
         />
       </div>
-      <div className={styles.burger}>
+      <div className={styles.burger} onClick={handleBurgerClick}>
         <span className={styles.burgerLine}></span>
         <span className={styles.burgerLine}></span>
         <span className={styles.burgerLine}></span>
       </div>
-      <div className={styles.menu}>
-        <ul className={styles.menuList}>
-          <li className={styles.menuItem}>
-            <a href="#" className={styles.menuLink}>
-              Главное
-            </a>
-          </li>
-          <li className={styles.menuItem}>
-            <a href="#" className={styles.menuLink}>
-              Мой плейлист
-            </a>
-          </li>
-          <li className={styles.menuItem}>
-            <a href="../signin.html" className={styles.menuLink}>
-              Войти
-            </a>
-          </li>
-        </ul>
-      </div>
+      {isBurgerClicked && (
+        <div className={styles.menu}>
+          <ul className={styles.menuList}>
+            <li className={styles.menuItem}>
+              <a href="#" className={styles.menuLink}>
+                Главное
+              </a>
+            </li>
+            <li className={styles.menuItem}>
+              <a href="#" className={styles.menuLink}>
+                Мой плейлист
+              </a>
+            </li>
+            <li className={styles.menuItem}>
+              <a href="../signin.html" className={styles.menuLink}>
+                Войти
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 }
