cf0b02ca1cc6b79211a95b8426286873c59c7275
KatiaVasileva <eka.vasileva@gmail.com>
2024-09-09 23:02:40 +0500

    signin page is created and styled

---
+++ skypro-music/src/app/signin/page.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function SignIn() {
+  return (
+    <body suppressHydrationWarning={true}>
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <div className={styles.block}>
+            <form className={styles.formLogin} action="#">
+              <a href="../">
+                <div className={styles.logo}>
+                  <Image
+                    className={styles.logoImage}
+                    src="/img/logo_modal.png"
+                    alt="logo"
+                    width={113.33}
+                    height={43}
+                  />
+                </div>
+              </a>
+              <input
+                className={styles.inputFirstChild}
+                type="text"
+                name="login"
+                placeholder="Почта"
+              />
+              <input
+                className={styles.input}
+                type="password"
+                name="password"
+                placeholder="Пароль"
+              />
+              <button className={styles.enterButton}>
+                <a className={styles.enterButtonLink} href="">Войти</a>
+              </button>
+              <button className={styles.signupButton}>
+                <a className={styles.signupButtonLink} href="">Зарегистрироваться</a>
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>    
+    </body>
+  );
+}
