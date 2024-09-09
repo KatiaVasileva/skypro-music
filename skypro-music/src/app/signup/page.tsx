6defa518274d90c8cc664ea325118dc24e821637
KatiaVasileva <eka.vasileva@gmail.com>
2024-09-09 23:25:30 +0500

    signup page is created and styled

--- skypro-music/src/app/signup/page.tsx
+++ skypro-music/src/app/signup/page.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function SignIn() {
+export default function SignUp() {
   return (
     <body suppressHydrationWarning={true}>
       <div className={styles.wrapper}>
         <div className={styles.container}>
           <div className={styles.block}>
-            <form className={styles.formLogin} action="#">
+            <form className={styles.formLogin}>
               <a href="../">
                 <div className={styles.logo}>
                   <Image
@@ -32,16 +32,21 @@ export default function SignIn() {
                 name="password"
                 placeholder="Пароль"
               />
-              <button className={styles.enterButton}>
-                <a className={styles.enterButtonLink} href="">Войти</a>
-              </button>
+              <input
+                className={styles.input}
+                type="password"
+                name="password"
+                placeholder="Повторите пароль"
+              />
               <button className={styles.signupButton}>
-                <a className={styles.signupButtonLink} href="">Зарегистрироваться</a>
+                <a className={styles.signupButtonLink} href="">
+                  Зарегистрироваться
+                </a>
               </button>
             </form>
           </div>
         </div>
-      </div>    
+      </div>
     </body>
   );
 }
