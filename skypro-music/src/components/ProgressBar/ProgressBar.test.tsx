4effa7d1edb65653fc7806c7509785273d598f7b
KatiaVasileva <eka.vasileva@gmail.com>
2024-09-22 15:20:37 +0500

    snapshot test for progress bar

---
+++ skypro-music/src/components/ProgressBar/ProgressBar.test.tsx
@@ -0,0 +1,17 @@
+import { render } from "@testing-library/react";
+import ProgressBar from "./ProgressBar";
+import { ChangeEvent } from "react";
+
+test("renders correctly", () => {
+  const { asFragment } = render(
+    <ProgressBar
+      max={0}
+      value={0}
+      step={0}
+      onChange={function (event: ChangeEvent<HTMLInputElement>): void {
+        throw new Error("Function not implemented.");
+      }}
+    />
+  );
+  expect(asFragment()).toMatchSnapshot();
+});
