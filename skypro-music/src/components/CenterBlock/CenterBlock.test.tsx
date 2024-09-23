7b378025e874a9f215141d457c352777703f4006
KatiaVasileva <eka.vasileva@gmail.com>
2024-09-23 21:14:38 +0500

    center block tests

--- skypro-music/src/components/CenterBlock/CenterBlock.test.tsx
+++ skypro-music/src/components/CenterBlock/CenterBlock.test.tsx
@@ -1,14 +1,36 @@
 import ReduxProvider from "../../store/ReduxProvider";
 import CenterBlock from "./CenterBlock";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 describe("Track component", () => {
-  it("renders correctly", () => {
-    const { asFragment } = render(
-        <ReduxProvider>
+  it("renders a heading", () => {
+    render(
+      <ReduxProvider>
         <CenterBlock allTracks={[]} />
       </ReduxProvider>
     );
-    expect(asFragment()).toMatchSnapshot();
+
+    const heading = screen.getByRole("heading", { level: 2 });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("renders search", () => {
+    render(
+      <ReduxProvider>
+        <CenterBlock allTracks={[]} />
+      </ReduxProvider>
+    );
+
+    expect(screen.getByPlaceholderText("Поиск")).toBeInTheDocument();
+  });
+
+  it("renders correctly", () => {
+    const { container } = render(
+      <ReduxProvider>
+        <CenterBlock allTracks={[]} />
+      </ReduxProvider>
+    );
+    expect(container).toMatchSnapshot();
   });
 });
